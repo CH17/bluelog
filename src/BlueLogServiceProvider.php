5781e558269f8ea7a187545ccedba086555ae038
Mridul Bhattacharjee <ch17@Mriduls-MacBook-Pro.local>
2024-11-09 13:45:27 +0200

    migration case sensative issue fix

--- src/BlueLogServiceProvider.php
+++ src/BlueLogServiceProvider.php
@@ -10,10 +10,10 @@ class BlueLogServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->loadMigrationsFrom(__DIR__.'/database/migrations');
+        $this->loadMigrationsFrom(__DIR__.'/Database/Migrations');
 
         $this->publishes([
-            __DIR__.'/database/migrations' => database_path('migrations'),
+            __DIR__.'/Database/Migrations' => database_path('migrations'),
         ], 'blue-log-migrations');
 
          if ($this->app->runningInConsole()) {
